30d24ca8323678f3d14fcb1469ed1aac96c2a29d
gauravjain0007 <gauravdoongarwal@gmail.com>
2024-09-17 02:45:36 +0530

    Implementing Singleton Class

---
+++ src/main/java/com/study/oops/designpatterns/singleton/SingletonCaller.java
@@ -0,0 +1,19 @@
+package com.study.oops.designpatterns.singleton;
+
+public class SingletonCaller {
+    public static void main(String[] args) {
+        SingletonClassImpl instance1;
+        // Creating first instance of the SingletonClassImpl class
+        instance1 = SingletonClassImpl.getInstance();
+        System.out.println(instance1);
+
+        // Trying to create second instance of the SingletonClassImpl class
+        // But will return the same instance as of first
+        SingletonClassImpl instance2 = SingletonClassImpl.getInstance();
+        System.out.println(instance2);
+
+        if (instance1 == instance2)
+            System.out.println("Both the instances are same.");
+    }
+
+}
---
+++ src/main/java/com/study/oops/designpatterns/singleton/SingletonClassImpl.java
@@ -0,0 +1,26 @@
+package com.study.oops.designpatterns.singleton;
+
+public class SingletonClassImpl {
+
+    // Private Static Instance of the Singleton class
+    private static SingletonClassImpl instance;
+
+    // Private Constructor
+    private SingletonClassImpl() {
+    }
+
+    /**
+     * Public Static Accessor to create/return the instance (only 1).
+     * The {@code synchronized} keyword will make it Thread-safe.
+     * 
+     * @return SingletonClassImpl - instance
+     */
+    public static synchronized SingletonClassImpl getInstance() {
+        if (instance == null) {
+            instance = new SingletonClassImpl();
+        }
+
+        return instance;
+    }
+
+}
